669adf5c5b98084244a7d2b773e89c311daed6c6
Simon Bedard <JavaKhanStudio@Gmail.com>
2021-03-15 12:05:51 +0100

    Firts commit

---
+++ twitchSDK/src/main/java/com/twitch/sdk/twitchSDK/TwitchSdkApplication.java
@@ -0,0 +1,13 @@
+package com.twitch.sdk.twitchSDK;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TwitchSdkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TwitchSdkApplication.class, args);
+	}
+
+}
---
+++ twitchSDK/src/test/java/com/twitch/sdk/twitchSDK/TwitchSdkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitch.sdk.twitchSDK;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitchSdkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
